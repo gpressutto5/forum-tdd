8338008ffeca5a529763ecb27af25f5a77aafd2b
Guilherme Pressutto <gpressutto5@gmail.com>
2017-05-08 23:55:54 -0300

    Thread can create replies

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Thread;
 
 class RepliesController extends Controller
 {
-    //
+    public function store(Thread $thread)
+    {
+        $thread->addReply([
+            'body' => request('body'),
+            'user_id' => auth()->id()
+        ]);
+        return back();
+    }
 }
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -10,11 +10,29 @@ class ThreadTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected $thread;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
     /** @test */
     public function a_thread_has_a_creator()
     {
-        $thread = factory('App\Thread')->create();
+        $this->assertInstanceOf('App\User', $this->thread->creator);
+    }
 
-        $this->assertInstanceOf('App\User', $thread->creator);
+    /** @test */
+    public function a_thread_can_add_a_reply()
+    {
+        $this->thread->addReply([
+            'body' => 'Foobar',
+            'user_id' => 1
+        ]);
+
+        $this->assertCount(1, $this->thread->replies);
     }
 }
