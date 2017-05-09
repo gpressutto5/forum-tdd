4db445aa50421edd92a0539cf6eec548ff8b6aa4
Guilherme Pressutto <gpressutto5@gmail.com>
2017-05-08 22:43:58 -0300

    Showing replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -10,7 +10,29 @@
                     <div class="panel-body">
                         {{ $thread->body }}
                     </div>
+
+                    <div class="panel-footer">
+                        {{ $thread->created_at->diffForHumans() }}
+                    </div>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @foreach($thread->replies as $reply)
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            {{ $reply->body }}
+                        </div>
+
+                        <div class="panel-footer">
+                            {{ $reply->created_at->diffForHumans() }}
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
