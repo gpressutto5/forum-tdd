12b1373ccecedd1183080e922243145b088c8e31
Guilherme Pressutto <gpressutto5@gmail.com>
2017-05-23 00:09:58 -0300

    Validation

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -13,6 +13,9 @@ class RepliesController extends Controller
 
     public function store($channel_slug, Thread $thread)
     {
+        $this->validate(request(), [
+            'body' => 'required'
+        ]);
         $thread->addReply([
             'body' => request('body'),
             'user_id' => auth()->id()
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -30,4 +30,16 @@ class ParticipateInForumTest extends TestCase
             ->post('/threads/slug/1/replies', [])
             ->assertRedirect('/login');
     }
+
+    /** @test */
+    public function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path().'/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
+    }
 }
