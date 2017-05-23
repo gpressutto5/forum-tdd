21c78c24c731e1fe5ad8a0cb9155b76b96eb81a0
Guilherme Pressutto <gpressutto5@gmail.com>
2017-05-22 22:43:29 -0300

    Channels WIP

--- app/Thread.php
+++ app/Thread.php
@@ -27,4 +27,9 @@ class Thread extends Model
     {
         $this->replies()->create($reply);
     }
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
+    }
 }
