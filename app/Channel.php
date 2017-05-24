dbc8f1a831a3c6ca76d0d45855681b3e211f0b66
Guilherme Pressutto <gpressutto5@gmail.com>
2017-05-24 17:11:52 -0300

    Text color

--- app/Channel.php
+++ app/Channel.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
+    const CONTRAST_THRESHOLD = 186;
+
     public function getRouteKeyName()
     {
         return 'slug';
@@ -19,5 +21,24 @@ class Channel extends Model
     public function getColorAttribute()
     {
         return '#'.substr(md5($this->name), 0, 6);
+    }
+
+    public function getTextColorAttribute()
+    {
+        $color = $this->color;
+        $subColor = [
+            'r' => hexdec(substr($color, 1, 2)),
+            'g' => hexdec(substr($color, 3, 2)),
+            'b' => hexdec(substr($color, 5, 2))
+        ];
+
+
+        $average = $subColor['r']*0.299 + $subColor['g']*0.587 + $subColor['b']*0.114;
+
+        if ($average >= self::CONTRAST_THRESHOLD) {
+            return '#000000';
+        }
+
+        return '#ffffff';
     }
 }
--- resources/views/helpers/channel_badge.blade.php
+++ resources/views/helpers/channel_badge.blade.php
@@ -1,3 +1,6 @@
 <a href="/threads/{{ $thread->channel->slug }}">
-    <span class="label label-success pull-right" style="background-color: {{ $thread->channel->color }};">{{ $thread->channel->name }}</span>
+    <span class="label label-success pull-right" style="
+            background-color: {{ $thread->channel->color }};
+            color: {{ $thread->channel->textColor }};
+            ">{{ $thread->channel->name }}</span>
 </a>
